daa9150d109688d052c2cd09e037044a776653f9
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-11-09 13:37:09 -0300

    add elasticsearch cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,3 +37,30 @@ include_recipe 'java'
 # node #####################
 
 include_recipe 'nodejs'
+
+# elasticsearch ############
+
+elasticsearch_install 'elasticsearch' do
+  type :tarball
+  dir '/usr/local'
+  version '1.4.5'
+  owner 'vagrant'
+  group 'vagrant'
+  action :install
+end
+
+# ruby_block "add-elasticsearch-to-PATH" do
+#   block do
+#     file = Chef::Util::FileEdit.new('/home/vagrant/.profile')
+#     file.insert_line_if_no_match(
+#       "# Add elasticsearch to PATH",
+#       "\n# Add elasticsearch to PATH\nexport PATH=\"/usr/local/elasticsearch/bin:$PATH\""
+#     )
+#     file.write_file
+#   end
+# end
+
+# execute 'elasticsearch' do
+#   environment 'PATH' => "/usr/local/elasticsearch/bin:#{ENV['PATH']}"
+# end
+
