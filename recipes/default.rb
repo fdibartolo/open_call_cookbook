469f860dc2b68690df611136b28570074ee148ad
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-11-09 18:14:45 -0300

    refactor ruby instalation into own recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,34 +1,8 @@
-#
 # Cookbook Name:: open_call_cookbook
 # Recipe:: default
-#
-# Copyright (C) 2015 YOUR_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
-# git ######################
 
 include_recipe 'git::default'
-
-# ruby #####################
-
-include_recipe 'rbenv::default'
-include_recipe 'rbenv::ruby_build'
-
-rbenv_ruby '2.2.1' do
-  global true
-end
-
-rbenv_gem 'bundler' do
-  ruby_version '2.2.1'
-  version '1.9.1'
-end
-
-rbenv_gem 'foreman' do
-  ruby_version '2.2.1'
-  version '0.78.0'
-end
+include_recipe 'open_call_cookbook::ruby'
 
 # java #####################
 
---
+++ recipes/ruby.rb
@@ -0,0 +1,19 @@
+# Cookbook Name:: open_call_cookbook
+# Recipe:: ruby
+
+include_recipe 'rbenv::default'
+include_recipe 'rbenv::ruby_build'
+
+rbenv_ruby '2.2.1' do
+  global true
+end
+
+rbenv_gem 'bundler' do
+  ruby_version '2.2.1'
+  version '1.9.1'
+end
+
+rbenv_gem 'foreman' do
+  ruby_version '2.2.1'
+  version '0.78.0'
+end
