db472426b4c4717224b4125f091119e39f9724f2
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-11-10 16:00:55 -0300

    add elasticsearch recipe specs

---
+++ spec/elasticsearch_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'open_call::elasticsearch' do
+  let(:chef_run) do 
+    ChefSpec::SoloRunner.new do |node|
+      node.set['open_call']['elasticsearch_version'] = '9.8.7'
+      node.set['open_call']['elasticsearch_path'] = '/some/path'
+    end.converge(described_recipe)
+  end
+
+  # it 'installs given elasticsearch version' do
+  #   expect(chef_run).to install_package('elasticsearch').with(version: '9.8.7')
+  # end
+
+  it 'creates directory with mode 755' do
+    expect(chef_run).to create_directory('/etc/profile.d').with(mode: 00755)
+  end
+
+  describe 'creates file' do
+    it 'with given path in content' do
+      expect(chef_run).to create_file('/etc/profile.d/elasticsearch.sh').with(mode: 00755)
+    end
+
+    it 'with mode 755' do
+      expect(chef_run).to create_file('/etc/profile.d/elasticsearch.sh').
+        with_content(start_with('export PATH=/some/path'))
+    end
+  end
+end
