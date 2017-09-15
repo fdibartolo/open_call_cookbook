19007e06dacf59322ba7cba2c4d14eafe045f2c3
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2017-09-15 17:07:56 -0300

    remove elasticsearch cookbook dependecy in favor of bash

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -1,20 +1,42 @@
 # Cookbook Name:: open_call
 # Recipe:: elasticsearch
 
-elasticsearch_install 'elasticsearch' do
-  type :tarball
-  dir node['open_call']['elasticsearch_path']
-  version node['open_call']['elasticsearch_version']
-  owner 'vagrant'
-  group 'vagrant'
-  action :install
+# elasticsearch_user 'vagrant'
+
+# elasticsearch_install 'elasticsearch' do
+#   type 'tarball'
+#   dir node['open_call']['elasticsearch_path']
+#   version node['open_call']['elasticsearch_version']
+#   # owner 'vagrant'
+#   # group 'vagrant'
+#   action :install
+# end
+
+# directory '/etc/profile.d' do
+#   mode 00755
+# end
+
+# file '/etc/profile.d/elasticsearch.sh' do
+#   content "export PATH=#{node['open_call']['elasticsearch_path']}/elasticsearch/bin:$PATH"
+#   mode 00755
+# end
+
+bash 'get elasticseach tar' do
+  code "wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-#{node['open_call']['elasticsearch_version']}.tar.gz"
 end
 
-directory '/etc/profile.d' do
-  mode 00755
+bash 'extract tar' do
+  code "sudo tar -xzf elasticsearch-#{node['open_call']['elasticsearch_version']}.tar.gz -C #{node['open_call']['elasticsearch_path']}"
 end
 
-file '/etc/profile.d/elasticsearch.sh' do
-  content "export PATH=#{node['open_call']['elasticsearch_path']}/elasticsearch/bin:$PATH"
-  mode 00755
+bash 'remove version from dir' do
+  code "sudo mv #{node['open_call']['elasticsearch_path']}/elasticsearch-#{node['open_call']['elasticsearch_version']} #{node['open_call']['elasticsearch_path']}/elasticsearch"
+end
+
+file "elasticsearch-#{node['open_call']['elasticsearch_version']}.tar.gz" do
+  action :delete
+end
+
+bash 'bin ownership' do
+  code "sudo chown -R vagrant:vagrant #{node['open_call']['elasticsearch_path']}/elasticsearch/"
 end
